57fa49becd8b433ab7bef758d109182dd6345a3d
Giovanny González Baltazar <giovanny@mifiel.com>
2024-06-24 19:02:04 -0600

    finished logic

--- update.rb
+++ update.rb
@@ -1,3 +1,19 @@
+require 'json'
+
+# monkey patch
+class String  ;
+  def titleize
+    self.split(/ |\_/).map(&:capitalize).join(" ")
+  end
+
+  def clean
+    self.gsub(/[-_]/, " ")
+  end
+
+  def to_name
+    self.clean.titleize
+  end
+end
 
 puts "=============================================================="
 puts "This scrips it's to use a custom gpl list of palettes on aseprite." \
@@ -9,9 +25,33 @@ puts "==============================================================\n\n"
 files = Dir["./*.gpl"]
 
 question = "Found #{files.length} files with .gpl extension"
-# printf "\e[31m#{question}. Press 'y' to continue: \e[0m"
-# prompt = STDIN.gets.chomp
-# return unless prompt == 'y'
+printf "\e[31m#{question}. Press 'y' to continue: \e[0m"
+prompt = STDIN.gets.chomp
+return unless prompt == 'y'
 
 dirname = File.basename(Dir.getwd)
+author = "Cat Cup Borrowed"
+publisher = "nobody"
 
+palettes = files.map do |file|
+  {
+    id: File.basename(file, ".gpl").to_name,
+    path: file
+  }
+end
+
+full_data = {
+  "name": dirname,
+  "displayName": dirname.to_name,
+  "version": "1.0",
+  "author": { "name": author, "url": "" },
+  "publisher": publisher,
+  "categories": [
+    "Palettes"
+  ],
+  "contributes": {
+    "palettes": palettes
+  }
+}
+
+puts JSON.pretty_generate(full_data)
